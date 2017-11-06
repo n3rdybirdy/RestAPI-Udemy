ab38e4fbda8c407843e9b469f6880bd977950423
Susi Bauer <n3rdybirdy@gmail.com>
2017-11-06 18:05:29 +0000

    Updating app.py with some fixes

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     from db import db
     db.init_app(app)
     app.run(port=5000, debug=True)
